0fda96da7308763908211ebdb35376519a52ec51
ntiko <nmathaba@tracker.co.za>
2026-05-28 01:44:46 +0200

    init BibleScreen.kt

--- app/src/main/java/com/example/bibleapp/RootNavigation.kt
+++ app/src/main/java/com/example/bibleapp/RootNavigation.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.bibleapp.feature.bible.presentation.BibleScreen
+import com.example.bibleapp.feature.bible.presentation.BibleViewModel
 
 @Composable
 fun RootNavigation(modifier: Modifier = Modifier) {
@@ -19,7 +24,13 @@ fun RootNavigation(modifier: Modifier = Modifier) {
             startDestination = Destination.Home.route
         ) {
             composable(Destination.Home.route){
+                val viewModel: BibleViewModel = hiltViewModel()
+                val state by viewModel.state.collectAsStateWithLifecycle()
 
+                BibleScreen(
+                    state = state,
+                    event = viewModel::onEvent
+                )
             }
         }
     }
