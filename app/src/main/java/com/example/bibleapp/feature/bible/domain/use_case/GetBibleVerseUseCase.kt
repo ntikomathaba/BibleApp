8d20bc1b01caa2e8ec8bff2833ab56b8d803944b
ntiko <nmathaba@tracker.co.za>
2026-05-28 01:43:34 +0200

    added GetBibleVerseUseCase.kt

---
+++ app/src/main/java/com/example/bibleapp/feature/bible/domain/use_case/GetBibleVerseUseCase.kt
@@ -0,0 +1,25 @@
+package com.example.bibleapp.feature.bible.domain.use_case
+
+import com.example.bibleapp.core.result.BaseResult
+import com.example.bibleapp.feature.bible.domain.repo.BibleRepo
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import timber.log.Timber
+import javax.inject.Inject
+
+class GetBibleVerseUseCase @Inject constructor(
+    val bibleRepo: BibleRepo
+) {
+    operator fun invoke(query: String) = flow {
+        emit(BaseResult.Loading())
+        val result = bibleRepo.getVerse(query)
+        if (result.isSuccess()) {
+            emit(result)
+        } else if (result.isFailure()) {
+            emit(result)
+        }
+    }.catch { exception ->
+        Timber.e("Error: $exception")
+        emit(BaseResult.Failure(Error(exception)))
+    }
+}
