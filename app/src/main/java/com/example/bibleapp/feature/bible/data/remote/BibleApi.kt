761ffbd8c0de3790e6ddc68cafd9fc830d6e37e9
ntiko <nmathaba@tracker.co.za>
2026-05-28 12:06:24 +0200

    added remote getBooks() Api

--- app/src/main/java/com/example/bibleapp/feature/bible/data/remote/BibleApi.kt
+++ app/src/main/java/com/example/bibleapp/feature/bible/data/remote/BibleApi.kt
@@ -1,6 +1,7 @@
 package com.example.bibleapp.feature.bible.data.remote
 
 import com.example.bibleapp.feature.bible.data.remote.dto.BibleVerseResponseDto
+import com.example.bibleapp.feature.books.data.remote.dto.BibleBooksResponseDto
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -10,4 +11,9 @@ interface BibleApi {
     suspend fun getVerse(
         @Path("query") query: String
     ): Response<BibleVerseResponseDto>
+
+    @GET("data/{translation}")
+    suspend fun getBooks(
+        @Path("translation") translation: String = "web"
+    ): Response<BibleBooksResponseDto>
 }
